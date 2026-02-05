8bc11b840a19a51fe263a30f33fdfdd60030df04
Your Name <you@example.com>
2026-02-05 08:59:16 +0700

    Completed everything about the Login/Sign up page

--- Components/Buttons/button-component.tsx
+++ Components/Buttons/button-component.tsx
@@ -13,8 +13,30 @@ export const ButtonComponent: React.FC<ButtonProps> = ({
   onClick,
   disabled = false,
 }) => {
+const buttonStyle: React.CSSProperties = {
+  backgroundColor: "#ffffff",
+  color: "black",
+  width: "150px",
+  padding: "10px 20px",
+  border: "black solid 2px",
+  borderRadius: "10px",
+  cursor: disabled ? "not-allowed" : "pointer",
+  fontSize: "16px",
+  opacity: disabled ? 0.6 : 1,
+
+  display: "block",
+  margin: "0 auto",
+};
+
+
+
   return (
-    <button type={type} onClick={onClick} disabled={disabled}>
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      style={buttonStyle}
+    >
       {label}
     </button>
   );
--- app/Authentication/Login/page.tsx
+++ app/Authentication/Login/page.tsx
@@ -7,7 +7,7 @@ const LoginPage = () => {
       <div className="back-btn">←</div>
 
       <div className="logo">
-      <img src="/Images/tracker.png" alt="Attendance Tracker Logo" />
+      <img src="https://icons.veryicon.com/png/o/education-technology/smart-campus-2/class-attendance.png" alt="Attendance Tracker Logo" />
       </div>
 
       <form>
@@ -29,11 +29,12 @@ const LoginPage = () => {
 
       <div className="divider">Or continue with</div>
 
+      {/* Logo for Logins */}
       <div className="social-login">
-        <img src="/Images/google.png" alt="Google" />
-        <img src="/Images/apple.png" alt="Apple" />
-        <img src="/Images/facebook.png" alt="Facebook" />
-        <img src="/Images/twitter.png" alt="Twitter" />
+        <img src="https://logos-world.net/wp-content/uploads/2020/09/Google-Symbol.png" alt="Google" />
+        <img src="https://th.bing.com/th/id/R.fe099dad9c1908c2520be9c7c752f7ee?rik=%2b0riBowoEIIxeQ&pid=ImgRaw&r=0" alt="Apple" />
+        <img src="https://freepnglogo.com/images/all_img/facebook-logo.png" alt="Facebook" />
+        <img src="https://static.vecteezy.com/system/resources/previews/027/395/710/non_2x/twitter-brand-new-logo-3-d-with-new-x-shaped-graphic-of-the-world-s-most-popular-social-media-free-png.png" alt="Twitter" />
       </div>
     </div>
   );
